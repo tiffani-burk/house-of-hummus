0d1b64531f7bd74e9e34e0a314eef30a3e6e0c76
Tiffani Burk <greengurl1088@yahoo.com>
2022-07-04 13:05:33 -0500

    database updated

--- scripts/database.js
+++ scripts/database.js
@@ -125,11 +125,56 @@ const database = {
         price: 4
     }
 ],
-    customOrder: [{
+    customOrders: [{
         id: 1,
         mainDishId: 2,
         veggieId: 6,
         sideId: 6,
         timestamp: 368239445
     }]
+}
+
+//create and export getter functions
+export const getMainDish = () => {
+    return database.mainDishes.map(mainDish => ({...mainDish}))
+}
+export const getVeggies = () => {
+    return database.veggies.map(veggie =>  ({...veggie}))
+}
+export const getSides = () => {
+    return database.sides.map(side => ({...side}))
+}
+export const getOrders = () => {
+    return database.customOrders.map(customOrder => ({...customOrder}))
+}
+
+//create and export setter functions that willl set state 
+const setMainDish = (id) => {
+    return database.tempOrder.mainDishId = id
+}
+const setVeggies = (id) => {
+    return database.tempOrder.veggieId = id
+}
+const setSides =(id) => {
+    return database.tempOrder.sideId = id
+}
+
+//create and export a function whose role is to take the temp state and make it permanant
+export const customOrder = () => {
+    const newOrder = {...database.tempOrder} //copy current state of user choices
+
+    //add a new primary key to the object
+    const lastIndex = database.customOrders.length - 1
+
+    //add a timestamp to the order
+    newOrder.customOrder.timestamp = Date.now()
+
+    //add a newOrder object to the custom order state by pushing the newOrder into the customOrders in the db
+    database.customOrders.push(newOrder)
+
+    //reset the temp state for the users
+    database.tempOrder = {}
+
+    //Broadcast notification that the perm state has changed 
+    document.dispatchEvent(new CustomEvent("stateChanged"))
 }
