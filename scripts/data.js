76447b7b4475bfba23a46a3d258cc612d86845da
Tiffani Burk <greengurl1088@yahoo.com>
2022-07-04 12:42:57 -0500

    first commit, database added

---
+++ scripts/data.js
@@ -0,0 +1,135 @@
+const database = {
+    tempOrder:{}, //leave empty to fill with later
+
+    mainDishes: [{
+        id: 1,
+        name: 'Hummus and Hot Sauce',
+        price: 8
+    },
+    {
+        id: 2,
+        name: 'Chicken Fried Lamb Kabob',
+        price: 18
+    },
+    {
+        id: 3,
+        name: 'Hot Chicken Greek Salad',
+        price: 16
+    },
+    {
+        id: 4,
+        name: 'Brussel Sprout Mousakka',
+        price: 12
+    },
+    {
+        id: 5,
+        name: 'Okrakopita',
+        price: 10
+    },
+    {
+        id: 6,
+        name: 'Fried Onion and Grape Leaves',
+        price: 9
+    },
+    {
+        id: 7,
+        name: 'Chess Baklava',
+        price: 6
+    },
+    {
+        id: 8,
+        name: 'Gyro Bisquits',
+        price: 16
+    },
+    {
+        id: 9,
+        name: 'Black Eyed Pea Falafal',
+        price: 11
+    },
+    {
+        id: 10,
+        name: 'Pecan Pastitsio',
+        price: 15
+    },
+],
+    sides: [{
+        id: 1,
+        name: 'Chicken Fried Steak',
+        price: 7
+    },
+    {
+        id: 2,
+        name: 'Bacon',
+        price: 8
+    },
+    {
+        id: 3,
+        name: 'Turkey Leg',
+        price: 7
+    },
+    {
+        id: 4,
+        name: 'Ribs',
+        price: 12
+    },
+    {
+        id: 5,
+        name: 'Catfish',
+        price: 8
+    },
+    {
+        id: 6,
+        name: 'Souvlaki',
+        price: 6
+    }
+
+],
+    veggies: [{
+        id: 1,
+        name: 'Okra',
+        price: 4
+    },
+    {
+        id: 2,
+        name: 'Callard Greens',
+        price: 4
+    },
+    {
+        id: 3,
+        name: 'Swiss Chard',
+        price: 4
+    },
+    {
+        id: 4,
+        name: 'Corn',
+        price: 4
+    },
+    {
+        id: 5,
+        name: 'Brussel Sprouts',
+        price: 4
+    },
+    {
+        id: 6,
+        name: 'Sweet Potatoes',
+        price: 4
+    },
+    {
+        id: 7,
+        name: 'Grits',
+        price: 4
+    },
+    {
+        id: 8,
+        name: 'Fried GreenTomatoes',
+        price: 4
+    }
+],
+    customOrder: [{
+        id: 1,
+        mainDishId: 2,
+        veggieId: 6,
+        sideId: 6,
+        timestamp: 368239445
+    }]
+}
